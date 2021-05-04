f4d4ba1d9787e298c263033c0a706fe24207c69e
franklindyer <franklin@dyer.me>
2021-05-03 20:46:49 -0600

    initial setup

---
+++ extruder_turtle/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+__title__ = 'extruder-turtle'
+__version__ = '0.1'
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+from extruder_turtle import __version__
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name='extruder-turtle',
+    version=__version__,
+    description='A Python package that uses the principles of Turtle Geometry to generate GCODE for 3d-printed solids.',
+    author='Franklin Pezzuti Dyer',
+    author_email='franklin+extruderturtle@dyer.me',
+    packages=find_packages(exclude=('docs', 'tests')),
+    include_package_data=True,
+    package_requires=[
+        'numpy',
+        'os'
+    ]
+    )
